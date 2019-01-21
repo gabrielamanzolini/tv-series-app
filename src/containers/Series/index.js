2e6180b6b8b01b77e178738e695db6147ef2b76a
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-01-21 15:42:09 -0200

    Fetching shows from API

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
 import SeriesList from '../../components/SeriesList';
+import Loader from '../../components/Loader';
 
 class Series extends Component {
     state = {
-        series: []
+        series: [],
+        seriesName: '',
+        isFetching: false
     }
-    componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=Grey%27s+Anatomy').then((response) => response.json())
-        .then(json => this.setState({series: json}))
+    onSeriesInputChange = e => {
+        this.setState({seriesName: e.target.value, isFetching: true})
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+            .then((response) => response.json())
+            .then(json => this.setState({series: json, isFetching: false}))
     }
     render() {
+        const {series, seriesName, isFetching} = this.state;
         return (
             <div>
-                The length of series array - {this.state.series.length}
+                <div>
+                    <input value={seriesName} type="text" onChange={this.onSeriesInputChange}/>
+                </div>
+                {
+                    !isFetching && series.length === 0 && seriesName.trim() ===''
+                    &&
+                    <p>Please enter series name into the input</p>
+                }
+                {
+                    !isFetching && series.length === 0 && seriesName.trim() !== ''
+                    &&
+                    <p>No TV Series have been found with this name</p>
+                }
+                {
+                    isFetching && <Loader/>
+                }
+                {
+                    !isFetching && <SeriesList list={this.state.series}/>
+                }
                 <SeriesList list={this.state.series}/>
             </div>
             
