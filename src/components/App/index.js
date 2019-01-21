dc85038f2112f6fa1d411893b31f5c8c2690f3dd
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-01-21 15:08:30 -0200

    Creation of SeriesList

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import Intro from '../Intro'
+import Series from '../../containers/Series'
 import './App.css';
 import "whatwg-fetch"
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Grey%27s+Anatomy').then((response) => {console.log(response)})
-  }
-
   render() {
     return (
       <div className="App">
@@ -18,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
+        <Series/>
       </div>
     );
   }
